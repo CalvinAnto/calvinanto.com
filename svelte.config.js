845c4765262e8b89012b2ee8010554a47870c1ee
calvinanto <contact@calvinanto.com>
2023-01-02 10:49:56 +0700

    output cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ out: '.svelte-kit/cloudflare'})
+		adapter: adapter()
 	}
 };
 
